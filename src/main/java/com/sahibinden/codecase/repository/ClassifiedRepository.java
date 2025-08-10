befad615caccab4d18a7baa9853ec44ebcb4f965
Yigit Ak <ak.yigitmesut@gmail.com>
2025-08-10 10:46:47 +0300

    classified repository added

---
+++ src/main/java/com/sahibinden/codecase/repository/ClassifiedRepository.java
@@ -0,0 +1,10 @@
+package com.sahibinden.codecase.repository;
+
+import com.sahibinden.codecase.entity.Classified;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClassifiedRepository extends JpaRepository<Classified, Long> {
+    Optional<Classified> findById(Long id);
+}
